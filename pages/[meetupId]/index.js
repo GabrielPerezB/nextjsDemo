1a4ac264571f804796e502e920a028bb6b59a7cb
gperez1800 <gperez@1800flowers.com>
2022-12-27 15:53:42 -0600

    dynamic pages getstaticprops & getstaticpaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ const MeetupDetails = () => {
   );
 };
 
+// used on dynamic pages to tell nextJS for which dynamic parameter values this page should be pre-generated
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg",
+        title: "A First Meetup",
+        description: "The meet description",
+        address: "Some street 5, Some City",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -22,14 +22,32 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-// this is for pre-render with async data
+// this is for pre-render with async data, always on server not on client side
 export async function getStaticProps() {
-  //fetch data from an API
+  //fetch data from an API ...
   return {
     props: {
       meetups: DUMMY_MEETUPS
-    }
+    },
+    // this will be re-generated certain amount of seconds at server-side after is build/deployed, 
+    // in order to fetch updated data
+    revalidate: 10
   };
 }
+
+// // this is for pre-render with async data, always on server not on client side
+// export async function getServerSideProps(context) {
+//   //fetch data from an API ...
+//   // This runs for every incoming request, don't needs revalidate 
+
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// }
 
 export default HomePage;
