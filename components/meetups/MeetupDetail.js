2b5c6da6a196d139d800ddd40d9de5026fb6d05d
gperez1800 <gperez@1800flowers.com>
2022-12-28 12:02:38 -0600

    optimize using next image

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import classes from "./MeetupDetail.module.css";
 
 const MeetupDetail = ({ image, title, description, address }) => {
   return (
     <section className={classes.detail}>
-      <img src={image} alt="A first meetup" />
+      <Image src={image} alt={title} />
       <h1>{title}</h1>
       <address>{address}</address>
       <p>{description}</p>
