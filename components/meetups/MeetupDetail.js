824b9c00446197ef569f3154bc8b8786f664dcb2
gperez1800 <gperez@1800flowers.com>
2022-12-28 12:18:02 -0600

    fix next image size

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -4,7 +4,7 @@ import classes from "./MeetupDetail.module.css";
 const MeetupDetail = ({ image, title, description, address }) => {
   return (
     <section className={classes.detail}>
-      <Image src={image} alt={title} />
+      <Image src={image} alt={title} layout='fill'/>
       <h1>{title}</h1>
       <address>{address}</address>
       <p>{description}</p>
